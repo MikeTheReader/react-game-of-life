6d0816c5084a60936d41a15b4ae3b1e7dd124c4b
MikeTheReader <MLundin617@gmail.com>
2017-03-21 21:35:22 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './css/normalize.css';
+import './css/skeleton.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
