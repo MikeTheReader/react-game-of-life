29d9a1bdcfb6cb8d6979a8e60dc8228645931bda
Michael Lundin <mikel@ifit.com>
2018-09-11 08:07:03 -0700

    Reformat files.
    Why:
    
    *
    
    Changed eslint rules, updated files to match. Also
    renamed JSX files and removed redundant App.
    
    *

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,52 @@
-
-import React, { Component } from 'react';
-import { generateRandomWorld, advance } from './life';
-import World from './World';
-import Counter from './Counter';
+import React, { Component } from "react";
+import { generateRandomWorld, advance } from "./life";
+import World from "./World";
+import Counter from "./Counter";
 
 const HEIGHT = 40;
 const WIDTH = 40;
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            world: generateRandomWorld(HEIGHT, WIDTH),
-            active: true,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      world: generateRandomWorld(HEIGHT, WIDTH),
+      active: true
+    };
+  }
 
-    componentDidMount() {
-        setTimeout(() => this.moveAStep(), 10);
-    }
+  componentDidMount() {
+    setTimeout(() => this.moveAStep(), 10);
+  }
 
-    togglePause = () => {
-        this.setState({
-            world: this.state.world,
-            active: !this.state.active,
-        });
-    }
+  togglePause = () => {
+    this.setState({
+      world: this.state.world,
+      active: !this.state.active
+    });
+  };
 
-    moveAStep() {
-        if (this.state.active) {
-            this.setState({
-                world: advance(this.state.world),
-                active: this.state.active,
-            });
-        }
-        setTimeout(() => this.moveAStep(), 10);
+  moveAStep() {
+    if (this.state.active) {
+      this.setState({
+        world: advance(this.state.world),
+        active: this.state.active
+      });
     }
+    setTimeout(() => this.moveAStep(), 10);
+  }
 
-    render() {
-        const buttonText = (this.state.active) ? 'Pause' : 'Play';
-        return (
-            <div className="container">
-                <h1>React Game of Life</h1>
-                <Counter world={this.state.world} />
-                <button onClick={this.togglePause}>{buttonText}</button>
-                <World world={this.state.world} />
-            </div>
-        );
-    }
+  render() {
+    const buttonText = this.state.active ? "Pause" : "Play";
+    return (
+      <div className="container">
+        <h1>React Game of Life</h1>
+        <Counter world={this.state.world} />
+        <button onClick={this.togglePause}>{buttonText}</button>
+        <World world={this.state.world} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/tests/life.test.js
+++ src/tests/life.test.js
@@ -1,117 +1,116 @@
-import { advance, generateRandomWorld, countLiveCells } from '../life';
+import { advance, generateRandomWorld, countLiveCells } from "../life";
 
-it('Obeys Rule One: Lone survivor dies', () => {
-    const worldStart = [
-        [0, 0, 0],
-        [0, 1, 0],
-        [0, 0, 0],
-    ];
-    const expected = [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ];
-    expect(advance(worldStart)).toEqual(expected);
+it("Obeys Rule One: Lone survivor dies", () => {
+  const worldStart = [
+    [0, 0, 0], 
+    [0, 1, 0], 
+    [0, 0, 0]
+  ];
+  const expected = [
+    [0, 0, 0], 
+    [0, 0, 0], 
+    [0, 0, 0]
+  ];
+  expect(advance(worldStart)).toEqual(expected);
 });
 
-it('Obeys rule One: Two individuals die', () => {
-    const worldStart = [
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-    ];
-    const expected = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ];
-    expect(advance(worldStart)).toEqual(expected);
+it("Obeys rule One: Two individuals die", () => {
+  const worldStart = [
+    [0, 1, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 1, 0, 0], 
+    [0, 0, 0, 0]
+  ];
+  const expected = [
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0]
+  ];
+  expect(advance(worldStart)).toEqual(expected);
 });
 
-
-it('Obeys Rule Two: Three neighbors live', () => {
-    const worldStart = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 0],
-    ];
-    const expected = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-    ];
-    expect(advance(worldStart)).toEqual(expected);
+it("Obeys Rule Two: Three neighbors live", () => {
+  const worldStart = [
+    [0, 0, 0, 0], 
+    [0, 1, 1, 0], 
+    [0, 0, 1, 0],
+    [0, 0, 0, 0]
+  ];
+  const expected = [
+    [0, 0, 0, 0], 
+    [0, 1, 1, 0], 
+    [0, 1, 1, 0], 
+    [0, 0, 0, 0]
+  ];
+  expect(advance(worldStart)).toEqual(expected);
 });
 
-it('Obeys Rule Three: Four neighbors die', () => {
-    const worldStart = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 1],
-    ];
-    const expected = [
-        [0, 0, 1, 0],
-        [0, 1, 1, 0],
-        [1, 1, 0, 1],
-        [0, 0, 1, 0],
-    ];
-    expect(advance(worldStart)).toEqual(expected);
+it("Obeys Rule Three: Four neighbors die", () => {
+  const worldStart = [
+    [0, 0, 0, 0], 
+    [0, 1, 1, 0], 
+    [0, 1, 1, 0], 
+    [0, 0, 0, 1]
+  ];
+  const expected = [
+    [0, 0, 1, 0], 
+    [0, 1, 1, 0], 
+    [1, 1, 0, 1], 
+    [0, 0, 1, 0]
+  ];
+  expect(advance(worldStart)).toEqual(expected);
 });
 
-it('Obeys Rule Four: Three neighbors breed life', () => {
-    const worldStart = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 0, 0],
-        [0, 0, 0, 0],
-    ];
-    const expected = [
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-    ];
-    expect(advance(worldStart)).toEqual(expected);
+it("Obeys Rule Four: Three neighbors breed life", () => {
+  const worldStart = [
+    [0, 0, 0, 0], 
+    [0, 1, 1, 0], 
+    [0, 1, 0, 0], 
+    [0, 0, 0, 0]
+  ];
+  const expected = [
+    [0, 0, 0, 0], 
+    [0, 1, 1, 0], 
+    [0, 1, 1, 0], 
+    [0, 0, 0, 0]
+  ];
+  expect(advance(worldStart)).toEqual(expected);
 });
 
-it('Generates a random world.', () => {
-    const zeroOrOne = {
-        asymmetricMatch: actual => actual === 0 || actual === 1,
-    };
-    let total = 0;
-    const testWorld = generateRandomWorld(20, 5);
-    expect(testWorld.length).toEqual(20);
-    expect(testWorld[0].length).toEqual(5);
-    testWorld.forEach((row) => {
-        expect(row.length).toEqual(5);
-        row.forEach((cell) => {
-            expect(cell).toEqual(zeroOrOne);
-            total += cell;
-        });
+it("Generates a random world.", () => {
+  const zeroOrOne = {
+    asymmetricMatch: actual => actual === 0 || actual === 1
+  };
+  let total = 0;
+  const testWorld = generateRandomWorld(20, 5);
+  expect(testWorld.length).toEqual(20);
+  expect(testWorld[0].length).toEqual(5);
+  testWorld.forEach(row => {
+    expect(row.length).toEqual(5);
+    row.forEach(cell => {
+      expect(cell).toEqual(zeroOrOne);
+      total += cell;
     });
-    expect(total).not.toEqual(20 * 5);
-    expect(total).not.toEqual(0);
+  });
+  expect(total).not.toEqual(20 * 5);
+  expect(total).not.toEqual(0);
 });
 
-it('Counts the live cells in a world.', () => {
-    const world = [
-        [0, 1, 1, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0],
-    ];
-    expect(countLiveCells(world)).toEqual(7);
+it("Counts the live cells in a world.", () => {
+  const world = [
+    [0, 1, 1, 0], 
+    [1, 1, 1, 1], 
+    [0, 0, 0, 1], 
+    [0, 0, 0, 0]
+  ];
+  expect(countLiveCells(world)).toEqual(7);
 
-    const zeroWorld = [
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0],
-    ];
-    expect(countLiveCells(zeroWorld)).toEqual(0);
+  const zeroWorld = [
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0]
+  ];
+  expect(countLiveCells(zeroWorld)).toEqual(0);
 });
