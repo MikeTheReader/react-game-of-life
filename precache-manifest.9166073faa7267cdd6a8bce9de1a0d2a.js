081be00af92e19d8f2c97824175b83cefbecb544
Michael Lundin <mikel@ifit.com>
2020-04-22 15:54:31 -0700

    Updates

---
+++ precache-manifest.9166073faa7267cdd6a8bce9de1a0d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91df3351e215aaabac0fcd7a02cb5047",
+    "url": "/react-game-of-life/index.html"
+  },
+  {
+    "revision": "c3df99d5744fe6817e16",
+    "url": "/react-game-of-life/static/css/main.f47e4c95.chunk.css"
+  },
+  {
+    "revision": "aab729661fe60c4779db",
+    "url": "/react-game-of-life/static/js/2.e59dcc67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-game-of-life/static/js/2.e59dcc67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3df99d5744fe6817e16",
+    "url": "/react-game-of-life/static/js/main.80f0a7c1.chunk.js"
+  },
+  {
+    "revision": "7fd2436d042e991f9008",
+    "url": "/react-game-of-life/static/js/runtime-main.f28f9348.js"
+  }
+]);
